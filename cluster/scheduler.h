8b2334c67623b6c374b6ba646aaebce79b65ba87
Will Dietz <wdietz2@illinois.edu>
2011-04-02 01:19:36 -0500

    Silly comment header change.   No functional changes.

--- cluster/scheduler.h
+++ cluster/scheduler.h
@@ -9,7 +9,7 @@
  *        Created:  03/31/2011 06:18:40 PM
  *
  *         Author:  Will Dietz (WD), wdietz2@illinios.edu
-                    Kevin Larson (KL), kevinlarson1@gmail.com
+ *                  Kevin Larson (KL), kevinlarson1@gmail.com
  *        Project:  Koala
  *
  * ===========================================================================
